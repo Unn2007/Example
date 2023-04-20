1ab59bc48c58bd0c15fbf141882c19decb5ab014
Unn2007 <118598200+Unn2007@users.noreply.github.com>
2023-04-20 17:20:21 +0000

    Deploying to gh-pages from @ Unn2007/Example@daa3965de8d97c8a0fa209f6fe78b196ef7e1b0a 🚀

--- index.ed98f089.js
+++
@@ -1,2 +0,0 @@
-var e;(e="Mango",new Promise(((o,c)=>{console.log(`Fetching data for ${e}`),setTimeout((()=>{o("success value")}),2e3)}))).then((e=>console.log(e))).catch((e=>console.error(e)));
-//# sourceMappingURL=index.ed98f089.js.map
