daa3965de8d97c8a0fa209f6fe78b196ef7e1b0a
Unn2007 <118598200+Unn2007@users.noreply.github.com>
2023-04-20 20:19:55 +0300

    ex

--- src/index.js
+++ src/index.js
@@ -1,20 +1,62 @@
-const fetchUserFromServer = username => {
-    return new Promise((resolve, reject) => {
-      console.log(`Fetching data for ${username}`);
+// const fetchUserFromServer = username => {
+//     return new Promise((resolve, reject) => {
+//       console.log(`Fetching data for ${username}`);
   
-      setTimeout(() => {
-        // Change value of isSuccess variable to simulate request status
-        const isSuccess = true;
+//       setTimeout(() => {
+//         // Change value of isSuccess variable to simulate request status
+//         const isSuccess = true;
   
-        if (isSuccess) {
-          resolve("success value");
-        } else {
-          reject("error");
-        }
-      }, 2000);
-    });
-  };
+//         if (isSuccess) {
+//           resolve("success value");
+//         } else {
+//           reject("error");
+//         }
+//       }, 2000);
+//     });
+//   };
   
-  fetchUserFromServer("Mango")
-    .then(user => console.log(user))
-    .catch(error => console.error(error));
+//   fetchUserFromServer("Mango")
+//     .then(user => console.log(user))
+//     .catch(error => console.error(error));
+const fetchUsersBtn = document.querySelector(".btn");
+const userList = document.querySelector(".user-list");
+
+fetchUsersBtn.addEventListener("click", () => {
+  fetchUsers()
+    .then((users) => renderUserList(users))
+    .catch((error) => console.log(error));
+});
+
+function fetchUsers() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
+function renderUserList(users) {
+  const markup = users
+    .map((user) => {
+      return `<li>
+          <p><b>Name</b>: ${user.name}</p>
+          <p><b>Email</b>: ${user.email}</p>
+          <p><b>Company</b>: ${user.company.name}</p>
+        </li>`;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
+
+const searchParams = new URLSearchParams({
+  _limit: 5,
+  _sort: "name",
+});
+console.log(searchParams);
+console.log(searchParams.toString()); // "_limit=5&_sort=name"
+
+const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
